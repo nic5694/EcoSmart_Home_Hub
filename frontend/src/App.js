01e0366239678d71e34c48b7c42f93380802f9d7
David Rallo <davidrallo28@gmail.com>
2023-11-02 11:17:58 -0400

    Base Of Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/youssef-chahboune/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Youssef Chahboune
-        </a>
-      </header>
-    </div>
+   <div>
+    <h1>
+      EcoSmart Home Hub 
+    </h1>
+   </div>
   );
 }
 
